b94d33b5c5ee8819748d95ed8c089dde448c0c9d
abuahmed <ibrayas@gmail.com>
2021-12-23 10:59:54 +0300

    updated packages

--- env.d.ts
+++ env.d.ts
@@ -9,3 +9,11 @@ declare global {
     dataLayer: any[];
   }
 }
+
+declare namespace NodeJS {
+  interface Process {
+    browser: boolean;
+  }
+}
+
+declare module "react-facebook-login/dist/facebook-login-render-props";
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== "production";
 console.log(dev);
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const port = process.env.port || 3000;
+const port = process.env.port || 5000;
 
 app.prepare().then(() => {
   createServer((req, res) => {
